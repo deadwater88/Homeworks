b7aba38c2ed02c9b113666e80704a081f6990abd
David Wong <david.wong.cal@gmail.com>
2017-03-30 22:43:45 -0700

    Did DIYADTS.rb homework

---
+++ W1D5/DIYADTS.rb
@@ -0,0 +1,66 @@
+class Stack
+
+  def initialize(arr = [])
+    @stack = arr
+  end
+
+  def add(el)
+    @stack.push(el)
+  end
+
+  def remove
+    @stack.pop
+  end
+
+  def show
+    @stack
+  end
+end
+
+class Queue
+  def initialize(arr =[])
+    @queue = arr
+  end
+
+  def enqueue(el)
+    @queue.unshift(el)
+  end
+
+  def dequeue
+    @queue.pop
+  end
+
+  def show
+    @queue
+  end
+
+end
+
+class Map
+  def initialize(map)
+    @map = map
+  end
+
+  def assign(key, value)
+    remove(key)
+    @map << [key, value]
+  end
+
+  def lookup(key)
+    @map.each do |k, v|
+      return v if k == key
+    end
+    nil
+  end
+
+  def remove(key)
+    @map.each_with_index do |k, v, index|
+      delete_at(index) if k == key
+    end
+  end
+
+  def show
+    @map
+  end
+
+end
